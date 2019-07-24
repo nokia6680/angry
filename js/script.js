b70bd3cb56342ba966a6adf01d4f35d1d1a3033b
Artur Ballod <artur.ballod@gmail.com>
2019-07-24 03:04:50 +0200

    1024px

--- js/script.js
+++ js/script.js
@@ -33,6 +33,36 @@ if (menuText) {
   });
 }
 
+/*Скрипт выезжающей штуки "Как участвовать"*/
+var modalSearch = document.querySelector(".top-panel");
+var closeButton = document.querySelector(".top-panel__close");
+var openButton = document.querySelectorAll(".open-btn");
+
+if (modalSearch) {
+  for (var i = 0; i < openButton.length; i++) openButton[i].addEventListener("click", function(event) {
+    event.preventDefault();
+    modalSearch.classList.add("top-panel--active");
+  });
+
+  modalSearch.addEventListener("click", function() {
+    modalSearch.classList.remove("top-panel--active");
+  });
+
+  modalSearch.addEventListener("click", function(event) {
+    event.stopPropagation();
+  });
+
+  closeButton.addEventListener("click", function() {
+    modalSearch.classList.remove("top-panel--active");
+  });
+
+  window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+      modalSearch.classList.remove("top-panel--active");
+    }
+  });
+}
+
 /*Слайдер персонажей*/
 $(document).ready(function() {
   $('.stars-slider').slick({
